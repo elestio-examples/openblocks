d3a75e1f8034f9a9982eee4a7e731ad276fdc121
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-03-08 17:32:06 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -9,13 +9,13 @@ docker-compose up -d;
 echo "Waiting for software to be ready ..."
 sleep 30s;
 
-target=$(docker-compose port openblocks 3000)
+# target=$(docker-compose port openblocks 3000)
 
-curl http://${target}/api/auth/form/login \
-  -H 'authority: openblocks-u353.vm.elestio.app' \
-  -H 'accept: application/json, text/plain, */*' \
-  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
-  -H 'content-type: application/json' \
-  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36' \
-  --data-raw '{"register":true,"loginId":"'${ADMIN_EMAIL}'","password":"'${ADMIN_PASSWORD}'","source":"EMAIL","authId":"EMAIL"}' \
-  --compressed
+# curl http://${target}/api/auth/form/login \
+#   -H 'authority: openblocks-u353.vm.elestio.app' \
+#   -H 'accept: application/json, text/plain, */*' \
+#   -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+#   -H 'content-type: application/json' \
+#   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36' \
+#   --data-raw '{"register":true,"loginId":"'${ADMIN_EMAIL}'","password":"'${ADMIN_PASSWORD}'","source":"EMAIL","authId":"EMAIL"}' \
+#   --compressed
